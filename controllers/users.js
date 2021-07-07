94cd2379c76ab629458e8e9cf5cec7b9bd2e0a76
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-07-07 19:48:12 +0500

    feat: обработать ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,108 @@
 const User = require('../models/user');
+const {
+  SUCCESS_OK,
+  ERROR_CODE,
+  NOT_FOUND,
+  ERROR_SERVER,
+} = require('../utils/status');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(SUCCESS_OK).send({ data: users }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.getUser = (req, res) => {
   const { userId } = req.body;
 
   User.findById({ userId })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(new Error('NotFound'))
+    .then((user) => res.status(SUCCESS_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные пользователя',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемый пользователь не найден',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(SUCCESS_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(new Error('NotFound'))
+    .then((user) => res.status(SUCCESS_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные при обновлении данных о пользователе',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемый пользователь не найден',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(new Error('NotFound'))
+    .then((user) => res.status(SUCCESS_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные при обновлении аватара',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемый пользователь не найден',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
---
+++ utils/status.js
@@ -0,0 +1,11 @@
+const SUCCESS_OK = 200;
+const ERROR_CODE = 400;
+const NOT_FOUND = 404;
+const ERROR_SERVER = 500;
+
+module.exports = {
+  SUCCESS_OK,
+  ERROR_CODE,
+  NOT_FOUND,
+  ERROR_SERVER,
+};
