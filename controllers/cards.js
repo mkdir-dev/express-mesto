73e8c7b29f1d3b5f046fbf998a8e9f53b752c36a
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-07-07 20:59:57 +0500

    feat: обработать ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,39 +1,120 @@
 const Card = require('../models/card');
+const {
+  SUCCESS_OK,
+  ERROR_CODE,
+  NOT_FOUND,
+  ERROR_SERVER,
+} = require('../utils/status');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(SUCCESS_OK).send({ data: cards }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  Card.create({
+    name,
+    link,
+    owner: req.user._id,
+  })
+    .then((card) => res.status(SUCCESS_OK).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные при создании карточки',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
 
   Card.findByIdAndRemove(cardId)
-    .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(new Error('NotFound'))
+    .then((card) => res.status(SUCCESS_OK).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные при удалении карточки',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемая карточка пользователя не найдена',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
   const { cardId } = req.params;
 
-  Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('NotFound'))
+    .then((card) => res.status(SUCCESS_OK).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные отметки "Мне нравится"',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемая карточка пользователя не найдена',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
 
-  Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  Card.findByIdAndUpdate(
+    cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('NotFound'))
+    .then((card) => res.status(SUCCESS_OK).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Переданы некорректные данные отметки "Мне нравится"',
+        });
+      }
+      if (err.name === 'NotFound') {
+        return res.status(NOT_FOUND).send({
+          message: 'Запрашиваемая карточка пользователя не найдена',
+        });
+      }
+      return res.status(ERROR_SERVER).send({
+        message: 'Ошибка сервера. Ошибка по-умолчанию',
+      });
+    });
 };
