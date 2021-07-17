daacae821bd2e035bb6efd36d256383ab8d9ca58
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-07-17 12:16:25 +0500

    feat: добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -17,6 +17,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 }, { versionKey: false });
 
 module.exports = mongoose.model('user', userSchema);
