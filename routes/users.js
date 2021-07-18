ccfc25e0163e2d198fc1a0be36274058e7d5cfa2
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-07-18 12:49:26 +0500

    refactor: удалить обработчик создания пользователя

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
   updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
-router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
