a4d53e7345d7a3edcaf24ad98372a8de288c7245
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-07-18 12:04:01 +0500

    feat: создать токен

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const {
   SUCCESS_OK,
@@ -129,24 +131,11 @@ module.exports.updateAvatar = (req, res) => {
 module.exports.login = (req, res) => {
   const { email, password } = req.body;
 
-  /*
-    User.findOne({ email })
-      .then((user) => {
-        if (!user) {
-          Promise.reject(new Error('Неправильные почта или пароль'));
-        }
-        return bcrypt.compare(password, user.password);
-      })
-      .then((matched) => {
-        if (!matched) {
-          Promise.reject(new Error('Неправильные почта или пароль'));
-        }
-        res.send({ message: 'Всё верно!' });
-      })
-      */
   return User.findUserByCredentials(email, password)
-    .then((/* user */) => {
-      // создать jwt token
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+
+      res.send({ token });
     })
     .catch(() => {
       res.status(ERROR_AUTH).send({
